2f5713e4c1a78672e5e10398e54986f04d3d9629
Alejandro <Alex_bcn10@hotmail.es>
2022-04-12 09:52:06 +0200

    created provided in index.js, and created store with reducer and imported devtoolsenhancer

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { devToolsEnhancer } from "redux-devtools-extension";
+import { createStore } from "redux";
+
+const initialState = {};
+
+const reducer = (state = initialState, action) => {
+    
+    return state;
+  };
+  
+  export default createStore(reducer, devToolsEnhancer());
+  
