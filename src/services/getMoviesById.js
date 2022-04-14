fe50d436e46e91decbf036e36fdd50b4839d168b
Alejandro <Alex_bcn10@hotmail.es>
2022-04-14 21:32:52 +0200

    changed api uri from the movie db to my backend iin getmoviesbyid

--- src/services/getMoviesById.js
+++ src/services/getMoviesById.js
@@ -1,7 +1,7 @@
 const getMoviesById = async (movie_id) => {
     console.log('llega a getMoviesid')
   const movie = await fetch(
-    `https://api.themoviedb.org/3/movie/${movie_id}?api_key=fb57b21a7894c177f41e137dd8b3e747&language=en-US`,
+    `https://api-restfull-movies-nodejs.herokuapp.com/movies/${movie_id}`,
     {
       method: "GET",
     }
