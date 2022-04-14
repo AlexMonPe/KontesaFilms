8141ec938cdc09da128afd7f14b0fd34e80a2f0c
Alejandro <Alex_bcn10@hotmail.es>
2022-04-14 21:41:19 +0200

    refactorized home including HomeMovies component and api changes in naming vars and functions

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,39 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import getPopularMovies from "../../services/getPopularMovies.js";
+import HomeMovies from "../../containers/HomeMovies/HomeMovies.js";
+import getMovies from "../../services/getMovies.js";
 import "./Home.css";
 
 const Home = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
   const navigate = useNavigate()
 
   useEffect(() => {
-    const loadPopularMovies = async () => {
+    const loadMovies = async () => {
       try {
-        const popularMovies = await getPopularMovies();
-        console.log(popularMovies, "popularmovies before setState");
-        setPopularMovies(popularMovies.results);
+        const movies = await getMovies();
+        console.log(movies, "movies before setState");
+        setMovies(movies);
       } catch (error) {
-        console.log(error, "Fatal error getting Popular Movies");
+        console.log(error, "Fatal error getting mMovies");
       }
     };
-    loadPopularMovies();
+    loadMovies();
   }, []);
-  const basePathImg = "http://image.tmdb.org/t/p/w200";
-  console.log(popularMovies, 'content of popular movies before return')
+  const basePathImg = "";
+  console.log(movies, 'content of popular movies before return')
   return (
     <div className="home-page">
-        <h3 className="text-black bg-amber-400 w-40 h-8 underline p-1 mb-6 font-bold text-center">POPULAR MOVIES</h3>
-      <div className="flex flex-row flex-wrap">
-          {popularMovies.map(movie => {
-            return(
-                <div className="popular-movies">
-                    <a className="cursor-pointer" href=""><img className="mb-4 hover:border-2 border-amber-400" src={`${basePathImg}/${movie.poster_path}`}></img></a>
-                    <button className="p-1 underline hover:text-white " onClick={()=> navigate("/detail/" + movie.id)}>{movie.title}</button>
-                </div>
-            )
-        })}
-      </div>
-      <div></div>
+        <h3 className="text-amber-400 bg-black w-40 h-8 underline p-1 mb-6 font-bold text-center border-amber-400 border">DRAMA</h3>
+        <HomeMovies movies={movies} text={"Drama"}/>
+        <h3 className="text-amber-400 bg-black w-40 h-8 underline p-1 mb-6 font-bold text-center border-amber-400 border">ANIMATION</h3>
+        <HomeMovies movies={movies} text={"Animation"}/>
+        <h3 className="text-amber-400 bg-black w-40 h-8 underline p-1 mb-6 font-bold text-center border-amber-400 border">SCIENCE FICTION</h3>
+        <HomeMovies movies={movies} text={"Science Fiction"}/>
+        <h3 className="text-amber-400 bg-black w-40 h-8 underline p-1 mb-6 font-bold text-center border-amber-400 border">THRILLER</h3>
+        <HomeMovies movies={movies} text={"Thriller"}/>
+        <h3 className="text-amber-400 bg-black w-40 h-8 underline p-1 mb-6 font-bold text-center border-amber-400 border">HORROR</h3>
+        <HomeMovies movies={movies} text={"Horror"}/>
     </div>
   );
 };
