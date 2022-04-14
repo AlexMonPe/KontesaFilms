a034b8f6ece1a058f55e8cc166e529386c77beca
Alejandro <Alex_bcn10@hotmail.es>
2022-04-14 21:31:05 +0200

    changed api url, removed function params, and removed use selector import

--- src/services/getRents.js
+++ src/services/getRents.js
@@ -1,12 +1,11 @@
-import { useSelector } from "react-redux";
-
-const getRents = async (idUser) => {
-  const rents = await fetch("http://localhost:4000/rent/byuser/" + idUser, {
-    method: "GET",
-  });
-  const rentsData = await rents.json();
-
-  return rentsData;
-};
-
-export default getRents;
+const getRents = async () => {
+    const rents = await fetch("https://api-restfull-movies-nodejs.herokuapp.com/rent/", {
+      method: "GET",
+    });
+    const rentsData = await rents.json();
+  
+    return rentsData;
+  };
+  
+  export default getRents;
+  
