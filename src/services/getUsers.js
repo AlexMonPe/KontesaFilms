e3fa58d9877bfd51536390614149ee5a6bfa2a82
Alejandro <Alex_bcn10@hotmail.es>
2022-04-15 22:06:09 +0200

    created getrents for dashboard

---
+++ src/services/getUsers.js
@@ -0,0 +1,11 @@
+const getUsers = async () => {
+    const users = await fetch("https://api-restfull-movies-nodejs.herokuapp.com/users/", {
+      method: "GET",
+    });
+    const usersData = await users.json();
+  
+    return usersData;
+  };
+  
+  export default getUsers;
+  
