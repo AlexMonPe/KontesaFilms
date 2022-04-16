7ce900b6c13ad60887a56dd3087787f910d2a5e7
Alejandro <Alex_bcn10@hotmail.es>
2022-04-16 21:40:28 +0200

    added a counter of items showed, and reset states

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Search from "../../components/Search/Search";
 import getMovies from "../../services/getMovies";
 import getRents from "../../services/getRents";
 import getUsers from "../../services/getUsers";
@@ -12,21 +13,21 @@ const Dashboard = () => {
   const showMovies = async () => {
     const moviesRes = await getMovies();
     setMovies(moviesRes);
-    setRents(undefined)
-    setUsers(undefined)
+    setRents(undefined);
+    setUsers(undefined);
   };
   const showRents = async () => {
     const rentsRes = await getRents();
     setRents(rentsRes);
-    setMovies(undefined)
-    setUsers(undefined)
+    setMovies(undefined);
+    setUsers(undefined);
     console.log(rentsRes, "data rentss");
   };
   const showUsers = async () => {
     const usersRes = await getUsers();
     setUsers(usersRes);
-    setMovies(undefined)
-    setRents(undefined)
+    setMovies(undefined);
+    setRents(undefined);
     console.log(usersRes, "data userss");
   };
   //useEffect(() => {}, []);
@@ -48,9 +49,23 @@ const Dashboard = () => {
           <a href="#" className="dashboardLinks" onClick={() => showUsers()}>
             ALL USERS
           </a>
-         {movies && (<><p className="text-white font-bold border-2 bg-red text-center">Total Movies: {movies.length}</p></>)} 
-         {rents && (<p className="text-white font-bold border-2 bg-red text-center">Total Rents: {rents.length}</p>)} 
-         {users && (<p className="text-white font-bold border-2 bg-red text-center">Total Users: {users.length}</p>)} 
+          {movies && (
+            <>
+              <p className="text-white font-bold border-2 bg-red text-center">
+                Total Movies: {movies.length}
+              </p>
+            </>
+          )}
+          {rents && (
+            <p className="text-white font-bold border-2 bg-red text-center">
+              Total Rents: {rents.length}
+            </p>
+          )}
+          {users && (
+            <p className="text-white font-bold border-2 bg-red text-center">
+              Total Users: {users.length}
+            </p>
+          )}
         </div>
         {movies && (
           <div className="flex flex-row flex-wrap gap-4 p-4 justify-center">
@@ -68,11 +83,25 @@ const Dashboard = () => {
             {rents.map((rent) => {
               return (
                 <div className="rentsCard">
-                  <p className="text-white bg-red p-2"><p>Email:</p> {rent.idUser.email}</p>
-                  <p className="text-white bg-red p-2"><p>Rent date:</p>{rent.rent_date}</p>
-                  <p className="text-white bg-red p-2"><p>Return date:</p>{rent.return_date}</p>
-                  <p className="text-white bg-red p-2"><p>Price: </p>{rent.totalPrice}</p>
-                  <p className="text-white bg-red p-2"><p>Title: </p>{rent.idMovie.title}</p>
+                  <p className="text-white bg-red p-2">
+                    <p>Email:</p> {rent.idUser.email}
+                  </p>
+                  <p className="text-white bg-red p-2">
+                    <p>Rent date:</p>
+                    {rent.rent_date}
+                  </p>
+                  <p className="text-white bg-red p-2">
+                    <p>Return date:</p>
+                    {rent.return_date}
+                  </p>
+                  <p className="text-white bg-red p-2">
+                    <p>Price: </p>
+                    {rent.totalPrice}
+                  </p>
+                  <p className="text-white bg-red p-2">
+                    <p>Title: </p>
+                    {rent.idMovie.title}
+                  </p>
                 </div>
               );
             })}
@@ -83,9 +112,17 @@ const Dashboard = () => {
             {users.map((user) => {
               return (
                 <div className="userCard">
-                  <p className="text-white bg-red p-2"><p>Name:</p> {user.name}</p>
-                  <p className="text-white bg-red p-2"><p>email:</p>{user.email}</p>
-                  <p className="text-white bg-red p-2"><p>Role:</p>{user.role}</p>
+                  <p className="text-white bg-red p-2">
+                    <p>Name:</p> {user.name}
+                  </p>
+                  <p className="text-white bg-red p-2">
+                    <p>email:</p>
+                    {user.email}
+                  </p>
+                  <p className="text-white bg-red p-2">
+                    <p>Role:</p>
+                    {user.role}
+                  </p>
                 </div>
               );
             })}
