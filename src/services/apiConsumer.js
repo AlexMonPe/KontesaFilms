7981bbbad6bfafb006370756b3a7b751781ece08
Alejandro <Alex_bcn10@hotmail.es>
2022-04-17 14:51:35 +0200

    added create user to apiconsumer

--- src/services/apiConsumer.js
+++ src/services/apiConsumer.js
@@ -1,20 +1,36 @@
 export const apiConsumer = {
-    loginUser: async (loginData) => {
-        try{
-            let loginUser = await fetch(
-                "https://api-restfull-movies-nodejs.herokuapp.com/users/login",
-                {
-                  method: "POST",
-                  body: JSON.stringify(loginData),
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                }
-              );
-              loginUser = await loginUser.json();
-              return loginUser
-        }catch(error){
-            console.log(error, 'error in login user apiconsumer')
+  loginUser: async (loginData) => {
+    try {
+      let loginUser = await fetch(
+        "https://api-restfull-movies-nodejs.herokuapp.com/users/login",
+        {
+          method: "POST",
+          body: JSON.stringify(loginData),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+      return await loginUser.json();
+    } catch (error) {
+      console.log(error, "error in login user apiconsumer");
     }
-}
+  },
+  createUser: async (formData) => {
+    try {
+      const createUser = await fetch(
+        "https://api-restfull-movies-nodejs.herokuapp.com/users/register",
+        {
+          method: "POST",
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return await createUser.json();
+    } catch (error) {
+      console.log(error, "Error in create user in apiconsumer");
+    }
+  },
+};
